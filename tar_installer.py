db26bb628307c09bc47ea2cdd1ba3e08857c65e9
Mikael Lucas <60666292+Mikaelluc@users.noreply.github.com>
2020-06-02 01:27:40 -0300

    Add suporte a .zip, .tar e .rar
    Além de solução de algumas inconsistências e mudanças nos textos.
    Ressalva que o formato desses 'elif' está feio e deve virar uma função, deve-se avaliar o formato, escrever a função e a forma de chamada dela

--- tar_installer.py
+++ tar_installer.py
@@ -4,24 +4,45 @@ import sys
 '''
 Script feito para automatizar a instalação de programas 
 compactados em alguma variante de .tar
+Compatível com 
 Falta terminar...
 '''
+def limpa_terminal():
+    system = os.uname().sysname
+    if(system.lower() == "linux"):
+        os.system('clear')
+    else:
+        os.system('cls')
+
+limpa = input("Limpar conteúdo anterior da tela [S/N]? ")
+if (limpa.lower() == 's'):
+    limpa_terminal()
+else:
+	pass
+
+
 try:
-    arq = sys.argv[1].split('.')
-    print('linha 12')
+    arq = sys.argv[1].split('.') 	#Se houver erro aqui, arg[1] inexistente, não foi informado arquivo para descompactação
 except:
     print('Não foi informado arquivo nenhum arquivo')
     sys.exit()
 
-if arq[len(arq)-1] == 'tar':
-    print('Arquivo .tar incompleto, sem especificação .gz ou .bz2 ou .xz')
-    sys.exit()
-
-if(arq[len(arq)-1] == 'bz2'):
-    os.system('tar -jxvf ' + arq)
+tipos = ['bz2', 'rar', 'tar', 'tar.bz2', 'tar.gz', 'tar.xz', 'zip']  #talvez possa ser procura na lista, ideia futura
+	
+if(arq[len(arq)-1] == 'rar'):
+    os.system('unrar x ' + arq)
+elif(arq[len(arq)-1] == 'tar'):
+    os.system('tar -xvf ' + arq)
+elif(arq[len(arq)-1] == 'bz2'):			# ressalva futura
+	if(arq[len(arq)-2] == 'tar'):
+    	os.system('tar -jxvf ' + arq)
+	else:
+		print("modelo " + sys.argv[1] + "ainda não suportado")
 elif(arq[len(arq)-1] == 'xz'):
     os.system('tar -Jxxvf ' + arq)
 elif(arq[len(arq)-1] == 'gz'):
     os.system('tar -zxvf ' + arq)
+elif(arq[len(arq)-1] == 'zip'):
+	os.system('unzip ' + arq)
 else:
-	print('Não é um arquivo formato .tar para instalação')
+	print('Não se enquadra nos parâmetros de descompactação')
